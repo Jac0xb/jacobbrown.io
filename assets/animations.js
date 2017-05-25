84116010a714aa022cd001e20ea3fca50ba2db09
Jacob Brown <jacobbrown41@outlook.com>
2017-05-25 00:48:53 -0600

    Added background animation, moved dancing icons.

--- assets/animations.js
+++ assets/animations.js
@@ -35,7 +35,8 @@ function funcAnimation() {
     // Creates floaters that traverse the whole of the webpage.
     function createFloaters() {
 
-        const randomImage = ["assets/images/silk_icons/application.png",
+        const randomImage = [
+            "assets/images/silk_icons/application.png",
             "assets/images/silk_icons/application_edit.png",
             "assets/images/silk_icons/application_xp_terminal.png",
             "assets/images/silk_icons/bug_link.png",
@@ -49,9 +50,9 @@ function funcAnimation() {
             "assets/images/silk_icons/wrench.png"];
 
         // JQuery generating an image elements.
-        const $floater = $('<img class="floaters" style="position:fixed; z-index: -10;  left:-20px; top:100px;" src="assets/images/silk_icons/star.png">');
+        const $floater = $('<img class="floaters" style="position:absolute; zoom: .8; -moz-transform: scale(0.8); z-index: 100;  left:-20px; top:100px;" src="assets/images/silk_icons/star.png">');
         $floater.attr('src', randomImage[RandomInteger(0, randomImage.length - 1)]);
-        $floater.appendTo($('#css-jacobbrown')[0]);
+        $floater.appendTo($('#main-banner')[0]);
 
         // Animations for floater.
         $floater.velocity({left: '102%'}, {duration: 20000, complete: function() {$floater.remove()}});
@@ -60,19 +61,22 @@ function funcAnimation() {
 
         function upAnimation() {
 
-            $floater.velocity({top: "60%"}, {queue: false, duration: 2000, complete: downAnimation});
+            $floater.velocity({top: "90%"}, {queue: false, duration: 2000, complete: downAnimation});
 
         }
 
         function downAnimation() {
 
-            $floater.velocity({top: "40%"}, {queue: false, duration: 2000, complete: upAnimation});
+            $floater.velocity({top: "70%"}, {queue: false, duration: 2000, complete: upAnimation});
 
         }
 
     }
 
 }
+
+
+
 
 // Runs when document is loaded.
 $(document).ready(function() {
@@ -157,6 +161,16 @@ $(document).ready(function() {
 
     });
 
+    /// Game Nav Button click event.
+    $("#main-nav-game").on("click", function() {
+        event.preventDefault();
+
+        window.history.pushState({}, 'jacobrown.io - Game', '?game');
+
+        AnimationLoadGame();
+
+    });
+
         /// Porfolio Nav Button click event.
         $("#main-nav-portfolio").on("click", function() {
             event.preventDefault();
@@ -177,7 +191,6 @@ $(document).ready(function() {
 
 
     });
-
 
     ///////////////////////////////////////////////////////////////////////////////////////
     // Animation for old portfolio page.
@@ -244,6 +257,70 @@ function milestoneOne() {
 }
  **/
 
+$("#background-code").ready(function() {
+
+    var $code = $("#background-code");
+    var modifiedHTML = "";
+    var absIndex = 0;
+    var newlineArrays = {};
+
+    var span = '<span style=\"display:inline-block; width: 25px;\"></span>';
+
+    $code.html().split('{end}').forEach(function(currLine, index, array) {
+
+        newlineArrays[index] = absIndex;
+
+        currLine.split('').forEach(function(currChar, index, array) {
+
+            if (currChar === '^')
+                currChar = span;
+
+            absIndex++;
+            modifiedHTML = modifiedHTML + '<span style="color:#F0F0F0; background-color:#F0F0F0;" id="background-code-node-' + absIndex + '\">' + currChar + '</span>';
+
+        });
+
+        modifiedHTML = modifiedHTML + "<br>";
+
+    });
+
+    $code.html(modifiedHTML);
+
+    var $codeLines = $("#background-code-line");
+
+    for (var line in newlineArrays) {
+        $codeLines.html($codeLines.html() + ( parseInt(line) + 1) + "<br>");
+    }
+
+    var floor = 1;
+    var elevator = 0;
+    var lengthOfSequence = -1;
+
+    var interval = setInterval(function() {
+
+        if (floor === absIndex)
+            clearInterval(this);
+
+        var $currNode = $("#background-code-node-" + floor);
+        $currNode.velocity({color:"#000"}, {queue: false, duration: 100});
+        $currNode.velocity({backgroundColor:["#F0F0F0","#000"]}, {queue: false, duration: 300});
+
+        floor++;
+
+    },200);
+
+});
+
+//
+//
+//
+function backgroundCodeAnimation() {
+
+    console.log("Load background");
+
+}
+
+
 var popupVisible = false;
 function createPDFPopup() {
 
@@ -251,7 +328,7 @@ function createPDFPopup() {
     // JQuery generating an image elements.
     const $pdfPopup = $(
         '<div class="popup"> <p><i>Click anywhere on the page to close.</i></p>' +
-        '<object data="assets/Brown,%20Jacob%20-%20Resume%20(5.22.2017).pdf" type="application/pdf" width="100%" height="100%">' +
+        '<object data="assets/Brown,%20Jacob%20-%20Resume%20(5.24.2017).pdf" type="application/pdf" width="100%" height="100%">' +
         '<p>Alternative text - include a link <a href="assets/Brown,%20Jacob%20-%20Resume%20(5.22.2017).pdf"">to the PDF!</a></p>' +
         '</object>' +
         '</div>');
@@ -276,29 +353,40 @@ $(".popup").click(function(e) {
     return false;
 });
 
-//
-//
-//
-$("#main-content").ready(function() {
+function grabParameter() {
 
     var directoryString = location.search.split('/');
 
     if(directoryString.length > 0) {
 
-        var explodedParameterString = directoryString[0].replace('?','');
+        var explodedParameterString = directoryString[0].replace('?', '').toLowerCase();
 
-        console.log(explodedParameterString);
-
-        if (explodedParameterString.toLowerCase() === "home")
-            AnimationLoadHome();
-        else if (explodedParameterString.toLowerCase() === "portfolio")
-            AnimationLoadPortfolio();
-        else if (explodedParameterString.toLowerCase() === "bio")
-            AnimationLoadBio();
-        else
-            AnimationLoadHome();
+        return explodedParameterString;
 
     }
+
+    return undefined;
+
+}
+
+//
+//
+//
+$("#main-content").ready(function() {
+
+     var parameterString = grabParameter();
+
+     if (parameterString === "home")
+         AnimationLoadHome();
+     else if (parameterString === "portfolio")
+         AnimationLoadPortfolio();
+     else if (parameterString === "bio")
+         AnimationLoadBio();
+     else if (parameterString === "game")
+         AnimationLoadGame();
+     else
+         AnimationLoadHome();
+
 });
 
 function AnimationLoadHome() {
@@ -308,6 +396,17 @@ function AnimationLoadHome() {
     $content.load("includes/main.html",function() {
 
         $("#page-main").velocity({opacity: 1.0}, {duration: 1000});
+    });
+
+}
+
+function AnimationLoadGame() {
+
+    var $content = $("#main-content");
+
+    $content.load("includes/game.html",function() {
+
+        $("#page-game").velocity({opacity: 1.0}, {duration: 1000});
     });
 
 }
